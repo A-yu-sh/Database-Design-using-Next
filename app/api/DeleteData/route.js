495d1a323538e4e949e9cfd70f66f524ac1f66e7
A-yu-sh <ayush.wd.dev@gmail.com>
2023-09-29 02:11:34 +0530

    Incomplete SWR changes

--- app/api/DeleteData/route.js
+++ app/api/DeleteData/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(request) {
   const id = request.nextUrl.searchParams.get("id");
+  console.log(id);
   await connectMongoDb();
   await IdentityModel.findByIdAndDelete(id);
   return NextResponse.json({ message: "Identity Deleted" });
--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import useSWR from "swr";
 
 export default function Home() {
+  const fetcher = async () => {
+    const res = await fetch("http://localhost:3000/api/GetData");
+    const data = await res.json();
+  };
   return (
     <main>
-      <h1 className="mt-5 text-xl flex justify-center">
-        THIS IS A NEW NEXT APP
-      </h1>
+      <h1 className="mt-5 text-xl flex justify-center">Identity Database</h1>
     </main>
   );
 }
