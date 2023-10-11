3a0e5640c3eac43b87a0f2afbf7a15ea58602384
A-yu-sh <ayush.wd.dev@gmail.com>
2023-10-11 12:51:06 +0530

    getting data from db

--- app/page.js
+++ app/page.js
@@ -1,22 +1,19 @@
-"use client";
 import Input from "./Components/Input";
 import { GET_IDENTITY } from "./api/AccessDB/route";
 import { signIn, signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
-export default function Home() {
-  // const data = await GET_IDENTITY();
-  const { status, data: session } = useSession();
+export default async function Home() {
+  const data = await GET_IDENTITY();
 
   return (
     <main>
       <h1 className="mt-5 text-xl flex justify-center">Listing</h1>
       <Input />
-      {status === "authenticated" ? (
-        <p>{session.user.name}</p>
-      ) : (
-        <button onClick={() => signIn("google")}>sign In</button>
-      )}
+      {data.data &&
+        data.data.map((e) => {
+          return <div>{e.task}</div>;
+        })}
     </main>
   );
 }
